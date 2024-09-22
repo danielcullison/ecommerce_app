9d9f64a66bcefe95f9d5896b0b70a1283884824e
DanielCullison <dcullison17@gmail.com>
2024-09-21 23:10:52 -0500

    Built Navbar.jsx, LatestCollection.jsx, and Hero.jsx components

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Hero from "../components/Hero";
+import LatestCollection from "../components/LatestCollection";
+
+export const Home = () => {
+  return <div>
+    <Hero />
+    <LatestCollection />
+  </div>;
+};
